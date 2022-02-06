ee17dd42af74587a040f6aed601cfe9378acc948
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-02-06 19:03:43 +0300

    Initial Commit

---
+++ CatchTheMoneys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheMoneys
+//
+//  Created by ugur-pc on 6.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
